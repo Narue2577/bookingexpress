780ccf70aae8d9a974f3e4e09c411abaf7aa332d
Narue2577 <67145851+Narue2577@users.noreply.github.com>
2026-09-02 17:17:11 +0700

    Update booking.js 2

--- routes/booking.js
+++ routes/booking.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+
+const roomData = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '../data/data.json'), 'utf8')
+);
+const roomsArray = Object.values(roomData);
 
 function requireAuth(req, res, next) {
   const token = req.cookies.token;
@@ -15,7 +22,7 @@ function requireAuth(req, res, next) {
 }
 router.get('/upload', (req, res) => res.render('upload'));
 router.get('/dashboard', requireAuth, (req, res) => {
-  res.render('dashboard', { user: req.user });
+  res.render('dashboard', { user: req.user , error: null, data: roomsArray  });
 });
 
 module.exports = router;
